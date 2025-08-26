5e61d04d66842ade5af54ba1428d1c603b58e95e
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-08-26 22:53:48 +0530

    selection

--- User Cards/src/App.jsx
+++ User Cards/src/App.jsx
@@ -2,34 +2,10 @@ import { useState } from 'react';
 import UserCard from './components/UserCard';
 import EditModal from './components/EditModal';
 import './App.css';
+import user from './data/user';
 
 function App() {
-  const [users, setUsers] = useState([
-    { 
-      id: 1, 
-      image: "https://picsum.photos/200/300",  
-      name: "Alice", 
-      age: 25, 
-      profession: "Designer",
-      description: "Creative designer with a passion for minimalism and user-friendly design."
-    },
-    { 
-      id: 2, 
-      image: "https://picsum.photos/300/300", 
-      name: "Bob", 
-      age: 30, 
-      profession: "Developer",
-      description: "Full-stack developer who loves building scalable web applications."
-    },
-    { 
-      id: 3, 
-      image: "https://picsum.photos/250/350", 
-      name: "Charlie", 
-      age: 28, 
-      profession: "Doctor",
-      description: "Dedicated doctor who believes in holistic patient care and wellness."
-    }
-  ]);
+  const [users, setUsers] = useState(user);
 
   const [selectedUser, setSelectedUser] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
---
+++ User Cards/src/data/user.js
@@ -0,0 +1,28 @@
+const user=[
+    { 
+      id: 1, 
+      image: "https://picsum.photos/200/300",  
+      name: "Alice", 
+      age: 25, 
+      profession: "Designer",
+      description: "Creative designer with a passion for minimalism and user-friendly design."
+    },
+    { 
+      id: 2, 
+      image: "https://picsum.photos/300/300", 
+      name: "Bob", 
+      age: 30, 
+      profession: "Developer",
+      description: "Full-stack developer who loves building scalable web applications."
+    },
+    { 
+      id: 3, 
+      image: "https://picsum.photos/250/350", 
+      name: "Charlie", 
+      age: 28, 
+      profession: "Doctor",
+      description: "Dedicated doctor who believes in holistic patient care and wellness."
+    }
+  ];
+
+  export default user;
