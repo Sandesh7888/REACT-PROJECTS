1717fb3e5304baeee28d3ffcbe7b18c939b125f7
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-08-29 22:14:03 +0530

    calculation

--- Calculator/src/components/Calculation.jsx
+++ Calculator/src/components/Calculation.jsx
@@ -1,64 +1,51 @@
 import React, { useState } from "react";
-import "./Calculation.css";
+import './Calculation.css';
+import ThemeToggle from "./ThemeToggle";
 
 const Calculation = () => {
-  const [value, setValue] = useState("");
-
-  // safe evaluate function
-  const calculate = () => {
-    try {
-      // eslint-disable-next-line no-new-func
-      const result = new Function("return " + value)();
-      setValue(result.toString());
-    } catch {
-      setValue("Error");
-    }
-  };
-
-  const handleClick = (e) => {
-    setValue(value + e.target.value);
-  };
+  const [value, setValue] = useState('');
+  const [theme, setTheme] = useState("light");
 
   return (
-    <div className="container">
+    <div className={`container ${theme}`}>
       <div className="calculator">
-        <div>
-          <input type="text" placeholder="0" value={value} id="result" disabled />
-        </div>
+        {/* Theme Toggle Switch */}
+        <ThemeToggle theme={theme} setTheme={setTheme} />
 
-        <div>
-          <input type="button" value="C" onClick={() => setValue("")} />
-          <input type="button" value="DE" onClick={() => setValue(value.slice(0, -1))} />
-          <input type="button" value="." onClick={handleClick} />
-          <input type="button" value="/" onClick={handleClick} />
-        </div>
-
-        <div>
-          <input type="button" value="7" onClick={handleClick} />
-          <input type="button" value="8" onClick={handleClick} />
-          <input type="button" value="9" onClick={handleClick} />
-          <input type="button" value="*" onClick={handleClick} />
-        </div>
-
-        <div>
-          <input type="button" value="4" onClick={handleClick} />
-          <input type="button" value="5" onClick={handleClick} />
-          <input type="button" value="6" onClick={handleClick} />
-          <input type="button" value="-" onClick={handleClick} />
-        </div>
-
-        <div>
-          <input type="button" value="1" onClick={handleClick} />
-          <input type="button" value="2" onClick={handleClick} />
-          <input type="button" value="3" onClick={handleClick} />
-          <input type="button" value="+" onClick={handleClick} />
-        </div>
-
-        <div>
-          <input type="button" value="0" onClick={handleClick} />
-          <input type="button" value="00" onClick={handleClick} />
-          <input type="button" value="=" onClick={calculate} />
-        </div>
+        <form name="form">
+          <div>
+            <input type="text" placeholder="0" value={value} id="result" disabled />
+          </div>
+          <div>
+            <input type="button" value="C" id="clear" onClick={() => setValue('')} />
+            <input type="button" value="DE" id="delete" onClick={() => setValue(value.slice(0, -1))} />
+            <input type="button" value="." id="decimal" onClick={(e) => setValue(value + e.target.value)} />
+            <input type="button" value="/" className="divide" onClick={(e) => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input type="button" value="7" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="8" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="9" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="*" onClick={(e) => setValue(value + e.target.value)} className="multiply" />
+          </div>
+          <div>
+            <input type="button" value="4" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="5" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="6" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="-" onClick={(e) => setValue(value + e.target.value)} className="subtract" />
+          </div>
+          <div>
+            <input type="button" value="1" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="2" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="3" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="+" onClick={(e) => setValue(value + e.target.value)} className="add" />
+          </div>
+          <div>
+            <input type="button" value="0" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="00" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="=" id="equals" onClick={() => setValue(eval(value))} />
+          </div>
+        </form>
       </div>
     </div>
   );
