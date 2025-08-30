b630cf8fab6a6d21aa8fb55935d89b5716fd774d
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-08-30 10:14:20 +0530

    calculation

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ThemeToggle from './components/ThemeToggle'
 import Calculation from './components/Calculation'
 
 function App() {
-  
+  const [theme, setTheme] = useState("light");
 
   return (
-    <>
+    <div className={`app ${theme}`}>
+      <ThemeToggle theme={theme} setTheme={setTheme} />
       <Calculation />
-    </>
-  )
+    </div>
+  );
 }
+
 
 
 export default App
