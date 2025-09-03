986af5d1eef5af887e42f13a48d33c8c9b3ba030
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-03 10:15:24 +0530

    form

---
+++ Registration Form/src/components/RegistrationForm.jsx
@@ -0,0 +1,55 @@
+import useForm from "../hooks/useForm.js";
+import  validate from "../hooks/validate.js";
+import React from "react";
+import 'RegistrationForm.css';
+const RegistrationForm = () => {
+  const { values, errors, handleChange, handleSubmit } = useForm(validate);
+
+  return (
+    <div className="form-container">
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        
+        {/* Name Field */}
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p className="error">{errors.name}</p>}
+        </div>
+
+        {/* Email Field */}
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+
+        {/* Password Field */}
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
