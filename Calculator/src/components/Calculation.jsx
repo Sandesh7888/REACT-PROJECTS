e1028c7e216bbc768c0a62fa3c186cad6a089f49
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-05 21:56:31 +0530

    new

--- Calculator/src/components/Calculation.jsx
+++ Calculator/src/components/Calculation.jsx
@@ -14,6 +14,26 @@ const Calculation = () => {
 
         <form name="form">
           <div>
+            <input type="text" placeholder="0" value={value} id="result" disabled />
+          </div>
+          <div>
+            <input type="button" value="C" id="clear" onClick={() => setValue('')} />
+            <input type="button" value="DE" id="delete" onClick={() => setValue(value.slice(0, -1))} />
+            <input type="button" value="." id="decimal" onClick={(e) => setValue(value + e.target.value)} />
+            <input type="button" value="/" className="divide" onClick={(e) => setValue(value + e.target.value)} />
+          </div>
+          <div>
+            <input type="button" value="7" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="8" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="9" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="*" onClick={(e) => setValue(value + e.target.value)} className="multiply" />
+          </div>
+          <div>
+            <input type="button" value="4" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="5" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="6" onClick={(e) => setValue(value + e.target.value)} className="number" />
+            <input type="button" value="-" onClick={(e) => setValue(value + e.target.value)} className="subtract" />
+          </div>
           <div>
             <input type="button" value="1" onClick={(e) => setValue(value + e.target.value)} className="number" />
             <input type="button" value="2" onClick={(e) => setValue(value + e.target.value)} className="number" />
