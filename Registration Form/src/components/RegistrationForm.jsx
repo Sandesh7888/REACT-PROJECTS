b940ecbc36e0e3612b180129f1762fa426e92108
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-07 19:41:42 +0530

    new

--- Registration Form/src/components/RegistrationForm.jsx
+++ Registration Form/src/components/RegistrationForm.jsx
@@ -36,12 +36,17 @@ const RegistrationForm = () => {
 
         {/* Password Field */}
         <div>
-
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
           />
           {errors.password && <p className="error">{errors.password}</p>}
         </div>
 
-      
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
