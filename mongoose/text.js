e134b738700116f97576a522d3e24c084c3858f5
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-16 22:37:08 +0530

    mongoose

--- mongoose/text.js
+++ mongoose/text.js
@@ -3,9 +3,49 @@
 //     x=x-typeof(solve)
 // }
 // console.log(x)
-console.log(x);
-console.log(s);
-var x=10;
-function s(){
-    console.log(x);
-};
+
+
+//hoisting
+// console.log(x);
+// console.log(s);
+// var x=10;
+// function s(){
+//     console.log(x);
+// };
+
+//shalo copy, deep copy
+// let x=[100,200,300];
+// let y=[100,200,300];
+// let z=y;
+// console.log(x==y);
+// console.log(z==y);
+// console.log(z==x);
+
+// console.log(typeof(null));
+// console.log(typeof(object));
+// console.log(typeof(undefined));
+// console.log(typeof(function(){}));
+
+
+// const obj={
+//     pqr:100,
+//     abc:200,
+//     xyz:{
+//         pqr:300,
+//         abc:400
+//     }
+// }
+// const {pqr,abc,xyz:{pqr:p}}=obj
+// console.log(pqr,abc,p);
+
+
+// let x=`hi`
+// console.log(`${x} welcome`);
+
+
+
+// let arr =[1,2,3,4,5]
+
+// let newarr = arr.map((a)=>{ return a>3;})
+// console.log(newarr);
+
