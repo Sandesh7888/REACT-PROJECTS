f7fc20cc517f255c9b14f6d48d9e84b690c4cede
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-09-18 22:01:24 +0530

    vanshaval 2.0

--- vanshval/backend/src/models/Member.js
+++ vanshval/backend/src/models/Member.js
@@ -4,7 +4,7 @@ const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   gender: { type: String, enum: ["male","female","other"], required: true },
   parent: { type: mongoose.Schema.Types.ObjectId, ref: "Member" },
-  family: { type: mongoose.Schema.Types.ObjectId, ref: "Family", required: true }
+  family: { type: mongoose.Schema.Types.ObjectId, ref: "Family", }
 }, { timestamps: true });
 
 export default mongoose.model("Member", memberSchema);
--- vanshval/backend/src/routes/members.js
+++ vanshval/backend/src/routes/members.js
@@ -3,6 +3,8 @@ import { addMember, getMembers } from "../controllers/memberController.js";
 import { protect } from "../middlewares/auth.js";
 const router = express.Router();
 
+
+
 router.get("/", protect, getMembers);
 router.post("/", protect, addMember);
 
